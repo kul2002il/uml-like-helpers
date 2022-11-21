6c1aca81db20815bd6ff70193e2056065a165084
Ilya V. Kulmanakov <ikulmanakov@tomsksoft.com>
2022-11-21 23:39:24 +0700

    Add classes for manual test. Add styles

--- public/main.js
+++ public/main.js
@@ -23,6 +23,11 @@ let classes = {
                 scope: 'private',
                 type: 'string',
             },
+            'subProject': {
+                name: 'subProject',
+                scope: 'private',
+                type: 'App/Models/Project',
+            },
             'manager': {
                 name: 'manager',
                 scope: 'private',
@@ -38,12 +43,107 @@ let classes = {
                 scope: 'public',
                 type: 'App/Models/Worker',
             },
+            'getManagerName': {
+                scope: 'public',
+                type: 'App/Models/Worker',
+            },
         },
         dependencies: [
             {
-                from: 'App/Models/Project',
-                to: 'App/Models/Worker',
+                fromClass: 'App/Models/Project',
+                fromProperty: 'manager',
+                toClass: 'App/Models/Worker',
                 type: 'aggregation',
+            },
+            {
+                fromClass: 'App/Models/Project',
+                formMethod: 'getManager',
+                toClass: 'App/Models/Worker',
+                type: 'association',
+            },
+            {
+                fromClass: 'App/Models/Project',
+                formMethod: 'getManagerName',
+                toClass: 'App/Models/Worker',
+                toMethod: 'getName',
+                type: 'association',
+            },
+            {
+                fromClass: 'App/Models/Project',
+                fromProperty: 'subProject',
+                toClass: 'App/Models/Project',
+                type: 'association',
+            },
+        ],
+    },
+    'TicTack/Class0': {
+        properties: {},
+        methods: {},
+        dependencies: [],
+    },
+    'TicTack/ComponentA/Class1': {
+        properties: {},
+        methods: {},
+        dependencies: [
+            {
+                fromClass: 'TicTack/ComponentA/Class1',
+                toClass: 'TicTack/Class0',
+                type: 'inheritance',
+            },
+        ],
+    },
+    'TicTack/ComponentB/Class2': {
+        properties: {},
+        methods: {},
+        dependencies: [
+            {
+                fromClass: 'TicTack/ComponentB/Class2',
+                toClass: 'TicTack/ComponentA/Class1',
+                type: 'inheritance',
+            },
+        ],
+    },
+    'TicTack/ComponentA/Class3': {
+        properties: {},
+        methods: {},
+        dependencies: [
+            {
+                fromClass: 'TicTack/ComponentA/Class3',
+                toClass: 'TicTack/ComponentB/Class2',
+                type: 'inheritance',
+            },
+        ],
+    },
+    'TicTack/ComponentB/Class4': {
+        properties: {},
+        methods: {},
+        dependencies: [
+            {
+                fromClass: 'TicTack/ComponentB/Class4',
+                toClass: 'TicTack/ComponentA/Class3',
+                type: 'inheritance',
+            },
+        ],
+    },
+    'TicTack/ComponentA/Class5': {
+        properties: {},
+        methods: {},
+        dependencies: [
+            {
+                fromClass: 'TicTack/ComponentA/Class5',
+                toClass: 'TicTack/ComponentB/Class4',
+                type: 'inheritance',
+            },
+        ],
+    },
+    'TicTack/Class6': {
+        properties: {},
+        methods: {},
+        dependencies: [
+            {
+                fromClass: 'TicTack/Class6',
+                toClass: 'TicTack/ComponentA/Class5',
+                type: 'inheritance',
             },
         ],
     },
@@ -115,7 +215,7 @@ let data = {
 const umlComponent = {
     props: ['component'],
     template: `<div class="component">
-        <div class="component-header"><div class="component-name">{{component.name}}</div></div>
+        <div class="component-header"><div v-if="component.name" class="component-name">{{component.name}}</div></div>
         <div class="component-body">
             <uml-component
             v-for="(subComponent, index) in component.components"
@@ -131,13 +231,24 @@ const umlComponent = {
 
 const umlClass = {
     props: ['classStruct'],
-    template: `<div class="class">
+    computed: {
+        // style: ()=>{return {
+        //     gridRow: 3,
+        //     gridColumn: 1,
+        // }},
+    },
+    template: `<div class="class" :style="style">
         <div class="class-name">{{classStruct.getName()}}</div>
         <hr>
         <div class="class-property"
         v-for="(property, index) in classStruct.getProperties()"
         v-bind:key="index"
         >{{property.name}}: {{property.type}}</div>
+        <hr>
+        <div class="class-method"
+        v-for="(method, index) in classStruct.getMethods()"
+        v-bind:key="index"
+        >{{method.name}}(): {{method.type}}</div>
     </div>`,
 };
 
