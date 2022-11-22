b9abde0a1f60ec148aa5e3dd790d862f55b7678e
Ilya V. Kulmanakov <ikulmanakov@tomsksoft.com>
2022-11-22 16:38:29 +0700

    Add resizable SVG under classes in UmlClassDiagram

--- public/js/Components/UmlClassDiagram.js
+++ public/js/Components/UmlClassDiagram.js
@@ -1,12 +1,36 @@
 
 const UmlClassDiagram = {
     props: ['classDiagramData'],
-    template: `<div class="component main-component">
-        <div class="component-body">
+    methods: {
+        getSvgElem() {
+            if (!this.$refs.mainComponent) {
+                this.$forceUpdate();
+                return {
+                    clientWidth:1,
+                    clientHeight:1,
+                }
+            }
+            return this.$refs.mainComponent
+        },
+        width() {
+            return this.getSvgElem().clientWidth;
+        },
+        height() {
+            return this.getSvgElem().clientHeight;
+        },
+        svgViewport() {
+            return '0 0 ' + this.width() + ' ' + this.height();
+        },
+    },
+    template: `<div class="main-component">
+        <div class="component-body" ref="mainComponent">
             <uml-class
             v-for="(classStruct, name, index) in classDiagramData.classes"
             v-bind:classStruct="classStruct"
             v-bind:key="name"></uml-class>
         </div>
+        <svg :viewBox="svgViewport()" :width="width()" :height="height()">
+            <circle cx="50" cy="50" r="40" />
+        </svg>
     </div>`,
 };
